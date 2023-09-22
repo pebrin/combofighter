d62ede5a5063e1c4832f95a13a60b24809166c13
pebrin <pebrin@gmail.com>
2023-09-22 01:13:18 -0400

    nevermind

--- app/server.py
+++ app/server.py
@@ -99,4 +99,4 @@ if __name__ == "__main__":
 		router = create_event_router(event)
 		socketio.on_event(event, router)
 	port = int(os.environ.get("PORT", 5000))
-	socketio.run(app, host='0.0.0.0', port=port)
+	socketio.run(app, host='0.0.0.0', port=port, debug=True)
