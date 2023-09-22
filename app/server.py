530d27aefe4e436a03f66427d3414286da3cfc6a
pebrin <pebrin@gmail.com>
2023-09-21 23:57:15 -0400

    changed requirements

--- app/server.py
+++ app/server.py
@@ -96,7 +96,5 @@ if __name__ == "__main__":
 	for event in active_event_types:
 		router = create_event_router(event)
 		socketio.on_event(event, router)
-	# port = int(os.environ.get("PORT", 5000))
-	port = int(os.environ.get("PORT", 8080))
+	port = int(os.environ.get("PORT", 5000))
 	socketio.run(app, host='0.0.0.0', port=port)
-	# socketio.run(app)
