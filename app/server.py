69e0c42584b13a056ef1f715f2f624b6ec671c4c
pebrin <pebrin@gmail.com>
2023-09-24 16:47:10 -0400

    removed 0.0.0.0 as host

--- app/server.py
+++ app/server.py
@@ -110,4 +110,5 @@ if __name__ == "__main__":
 		socketio.on_event(event, router)
 
 	port = int(os.environ.get("PORT", 5000))
-	socketio.run(app, host='0.0.0.0', port=port, debug=True)
+	# socketio.run(app, host='0.0.0.0', port=port, debug=True)
+	socketio.run(app, port=port, debug=True)
